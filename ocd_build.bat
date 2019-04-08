fbea8bd4450d25cf7e80c32a0af5682b706f9e14
Dirk Groeneveld <groeneveld@gmail.com>
2019-04-07 18:05:12 -0700

    Adds build script

---
+++ ocd_build.bat
@@ -0,0 +1,11 @@
+mkdir ocd_build_debug
+cd ocd_build_debug
+\Qt\5.9.7\msvc2015_64\bin\qmake.exe ..\QtAV.pro "CONFIG+=debug" "CONFIG+=config_swresample" "CONFIG*=d3dva"
+nmake
+cd ..
+
+mkdir ocd_build_release
+cd ocd_build_release
+\Qt\5.9.7\msvc2015_64\bin\qmake.exe ..\QtAV.pro "CONFIG+=release" "CONFIG+=config_swresample" "CONFIG*=d3dva"
+nmake
+cd ..
