bf92fa6f49f81f9a72ee73451b57d0e74d370736
Dirk Groeneveld <groeneveld@gmail.com>
2019-04-07 18:10:58 -0700

    Parallel builds

--- ocd_build.bat
+++ ocd_build.bat
@@ -1,11 +1,11 @@
 mkdir ocd_build_debug
 cd ocd_build_debug
 \Qt\5.9.7\msvc2015_64\bin\qmake.exe ..\QtAV.pro "CONFIG+=debug" "CONFIG+=config_swresample" "CONFIG*=d3dva"
-nmake
+C:\Qt\Tools\QtCreator\bin\jom.exe
 cd ..
 
 mkdir ocd_build_release
 cd ocd_build_release
 \Qt\5.9.7\msvc2015_64\bin\qmake.exe ..\QtAV.pro "CONFIG+=release" "CONFIG+=config_swresample" "CONFIG*=d3dva"
-nmake
+C:\Qt\Tools\QtCreator\bin\jom.exe
 cd ..
